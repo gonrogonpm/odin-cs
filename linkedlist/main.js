374161717728d9e6960d1cef2cfe5f4f0b7257df
gonrogon <gonrogon@gmail.com>
2025-03-30 20:01:19 +0200

    Move LinkedList tests to main.js

---
+++ linkedlist/main.js
@@ -0,0 +1,79 @@
+import { LinkedList } from "./linkedlist.js";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(`empty: ${list.empty}`);
+console.log(`size: ${list.size}`);
+console.log(`head: ${list.head}`);
+console.log(`tail: ${list.tail}`);
+console.log(`list: ${list.toString()}`);
+
+console.log('\n--- POP ONE ---\n');
+list.pop();
+
+console.log(`tail: ${list.tail}`);
+console.log(`list: ${list.toString()}`);
+console.log(`find: ${list.find('parrot')} - ${list.at(list.find('parrot'))}`);
+console.log(`find: ${list.find('dog')} - ${list.at(list.find('dog'))}`);
+console.log(`find: ${list.find('turtle')}`);
+
+console.log('\n--- POP ALL ---\n');
+list.pop();
+list.pop();
+list.pop();
+list.pop();
+list.pop();
+
+console.log(`empty: ${list.empty}`);
+console.log(`size: ${list.size}`);
+console.log(`head: ${list.head}`);
+console.log(`tail: ${list.tail}`);
+console.log(`list: ${list.toString()}`);
+
+console.log('\n--- INSERT AT INDEX 2 ---\n');
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(`list: ${list.toString()}`);
+list.insertAt('elephant', 2);
+console.log(`list: ${list.toString()}`);
+
+console.log('\n--- INSERT AT BEGINNING ---\n');
+
+console.log(`list: ${list.toString()}`);
+list.insertAt('mouse', 0);
+console.log(`list: ${list.toString()}`);
+
+console.log('\n--- INSERT AT END ---\n');
+
+console.log(`list: ${list.toString()}`);
+list.insertAt('bat', list.size);
+console.log(`list: ${list.toString()}`);
+
+console.log('\n--- INSERT OUT OF BOUNDS ---\n');
+
+try {
+    list.insertAt('bat', -1);
+}
+catch (error) {
+    console.log(`error: ${error}`);
+}
+
+try {
+    list.insertAt('bat', list.size + 1);
+}
+catch (error) {
+    console.log(`error: ${error}`);
+}
